48e5026ff93b35efa7d9fb22719b4f6af9dd3e73
Roger Cheng <roger.random@outlook.com>
2024-10-20 03:34:16 -0700

    Smaller STL file sizes with tolerance 25e-5

--- generate_all.py
+++ generate_all.py
@@ -98,8 +98,8 @@ for spool in spools:
             spool["inner"],
             spool["outer"],
             spool["height"],
-            size, label=False).rotate((0,0,0),(0,0,1),-90-size/2)
-        exporters.export(test_tray, filename, tolerance=1e-4)
+            size).rotate((0,0,0),(0,0,1),-90-size/2)
+        exporters.export(test_tray, filename, tolerance=25e-5)
 
         filename = "{:s}/placeholder_{:d}.stl".format(subdirectory, size)
         print(filename)
